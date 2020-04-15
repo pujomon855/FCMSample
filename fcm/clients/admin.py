1d7cd677fa7443372fb9d081279179fc58667bef
pujomon855 <pujomon855@gmail.com>
2020-04-15 14:58:46 +0900

    管理画面のカスタマイズ

--- fcm/clients/admin.py
+++ fcm/clients/admin.py
@@ -3,11 +3,41 @@ from django.contrib import admin
 from .models import Client, Code, CodeSession, CodeSessionProduct, HandlInst, Product, Session, SessionProduct
 
 
-admin.site.register(Client)
-admin.site.register(Code)
-admin.site.register(CodeSession)
-admin.site.register(CodeSessionProduct)
+class CodeInLine(admin.StackedInline):
+    model = Code
+
+
+class ClientAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'client_id']}),
+    ]
+    inlines = [CodeInLine]
+
+
+admin.site.register(Client, ClientAdmin)
+
+
+class CodeSessionInLine(admin.TabularInline):
+    model = CodeSession
+
+
+class SessionProductInLine(admin.TabularInline):
+    model = SessionProduct
+
+
+class CodeSessionProductInLine(admin.TabularInline):
+    model = CodeSessionProduct
+
+
+class SessionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Time information', {'fields': ['start_time', 'end_time']}),
+    ]
+    inlines = [CodeSessionInLine, SessionProductInLine, CodeSessionProductInLine]
+
+
+admin.site.register(Session, SessionAdmin)
+
 admin.site.register(HandlInst)
 admin.site.register(Product)
-admin.site.register(Session)
-admin.site.register(SessionProduct)
