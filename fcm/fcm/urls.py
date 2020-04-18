be0cde7c31a30ffc28721e3a3deffa609b1872a6
pujomon855 <pujomon855@gmail.com>
2020-04-18 21:56:00 +0900

    管理画面で使用されているタグなどのドキュメントを管理画面からみられるようにした

--- fcm/fcm/urls.py
+++ fcm/fcm/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('clients/', include('clients.urls')),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
 ]
