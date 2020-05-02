5e0965a4b9e294ae9b6d3bb5181495534558baaf
pujomon855 <pujomon855@gmail.com>
2020-05-02 21:08:53 +0900

    カラム名や値を動的に取得できるようにした

--- fcm/clients/views.py
+++ fcm/clients/views.py
@@ -11,12 +11,35 @@ class IndexView(generic.TemplateView):
 
 @dataclass
 class ClientTableRecord:
-    id: int
+    # key:カラム名、item: カラム名に紐づくこのクラスの属性値の取得関数
+    # itemの戻り値は、属性値自身とリンク先の情報(なければNone)のタプル
+    _CL_TAB_COL_VALUES = {
+        'Client Name': lambda record: (record.name, {'url': 'clients:detail', 'args': record.id}),
+        'Session': lambda record: (record.session, None),
+        'Session Start End': lambda record: (record.session_start_end, None),
+        'Code': lambda record: (record.code, None),
+        'Products': lambda record: (record.products, None),
+    }
+    _CL_TAB_COL_NAMES = _CL_TAB_COL_VALUES.keys()
+
+    id: str
     name: str
     session: str
     session_start_end: str
     code: str
     products: str
+
+    @classmethod
+    def column_names(cls):
+        """
+        顧客テーブルのカラム名のリストを返す
+        :return: 顧客テーブルのカラム名のリスト
+        """
+        return cls._CL_TAB_COL_NAMES
+
+    def __iter__(self):
+        for func in self._CL_TAB_COL_VALUES.values():
+            yield func(self)
 
 
 class ClientTableView(generic.ListView):
@@ -35,9 +58,15 @@ class ClientTableView(generic.ListView):
                     session_start_end = '-'.join((str(session.start_time), str(session.end_time)))
                     csp_list = TradeType.objects.filter(code=classifier).filter(session=session)
                     products = ', '.join([f'{csp.product}({csp.handlinst})' for csp in csp_list])
-                    record = ClientTableRecord(client.id, client.name, session, session_start_end, classifier, products)
+                    record = ClientTableRecord(
+                        client.id, client.name, session, session_start_end, classifier, products)
                     client_records.append(record)
         return client_records
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['col_names'] = ClientTableRecord.column_names()
+        return context
 
 
 def show_detail(request, pk):
