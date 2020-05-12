e02511177ef4d160874f361878872c0d0989db84
pujomon855 <pujomon855@gmail.com>
2020-05-12 16:29:05 +0900

    IEがString.startsWith()未対応のため、polyfillを作成(前のコミットでの追加漏れ)

---
+++ fcm/clients/static/admin/js/polyfill/string-polyfill.js
@@ -0,0 +1,15 @@
+/**
+ * Stringクラスのpolyfill。
+ * ほぼIE用。
+*/
+
+'use strict';
+
+if (!String.prototype.startsWith) {
+    Object.defineProperty(String.prototype, 'startsWith', {
+        value: function(search, rawPos) {
+            var pos = rawPos > 0 ? rawPos|0 : 0;
+            return this.substring(pos, pos + search.length) === search;
+        }
+    });
+}
