89c52aa1f1aff32780f0695105415e9de6849a5e
pujomon855 <pujomon855@gmail.com>
2020-05-18 17:48:58 +0900

    多言語対応
    urlで日本語-英語の切り替えができるようにした

--- fcm/fcm/urls.py
+++ fcm/fcm/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('clients/', include('clients.urls')),
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += i18n_patterns(
+    path('clients/', include('clients.urls')),
+)
